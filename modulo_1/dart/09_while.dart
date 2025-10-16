45c1378f26fe46b7b5f5668f6b570736540910b0
Carlos Sánchez <carlosasp-13@hotmail.com>
2025-10-15 19:56:32 -0500

    Clase_15_10_2025

---
+++ modulo_1/dart/09_while.dart
@@ -0,0 +1,16 @@
+void main() {
+ int n = 10;
+ int tabla = 3;
+ int i=0;
+ while (i<n){
+   
+   print("$i * $tabla = ${i*tabla}");
+   i++;
+ }
+    i=1;
+  do{
+   print("$i * $tabla = ${i*tabla}");
+   i++;
+ }while(i<n);
+  
+}
