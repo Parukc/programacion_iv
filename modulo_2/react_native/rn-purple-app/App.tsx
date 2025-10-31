7757b2658e05319f40cdbb7077300f066279a8ef
Carlos Sánchez <carlosasp-13@hotmail.com>
2025-10-30 21:46:34 -0500

    clase30/10/2025

---
+++ modulo_2/react_native/rn-purple-app/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import RootNavigator from "./src/navigation/RootNavigator";
+
+export default function App() {
+  return (
+    <RootNavigator />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
