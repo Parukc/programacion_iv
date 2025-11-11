5d9b06136dadaf96cb57bb3d1e3b2b51a5c3985e
Carlos Sánchez <carlosasp-13@hotmail.com>
2025-11-10 21:55:14 -0500

    Clase_10-11_2025

--- modulo_2/react_native/rn-purple-app/App.tsx
+++ modulo_2/react_native/rn-purple-app/App.tsx
@@ -1,11 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 import React from "react";
 import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
 import RootNavigator from "./src/navigation/RootNavigator";
+import 'react-native-reanimated';
 
 export default function App() {
   return (
-    <RootNavigator />
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <RootNavigator />
+    </View>
   );
 }
 
---
+++ modulo_2/react_native/rn-purple-app/src/services/auth.service.ts
@@ -0,0 +1,25 @@
+import api from "./api";
+import { authStore } from "../store/auth";
+import { Platform } from "react-native";
+
+// API pública: https://reqres.in
+// const BASE = "https://reqres.in/api";
+const host = Platform.OS === 'android' ? '10.0.2.2' : 'localhost';
+export const BASE = `http://${host}:8000/api/auth`;
+
+export async function login(email: string, password: string) {
+  const { data } = await api.post(`${BASE}/login`, { username: email, password });
+  authStore.set({ token: data.token, email });
+  return data;
+}
+
+export async function register(email: string, password: string) {
+  const { data } = await api.post(`${BASE}/register`, { email, password });
+  // Opcional: guardar token si devuelve
+  authStore.set({ token: data.token ?? null, email });
+  return data;
+}
+
+export function logout() {
+  authStore.clear();
+}
